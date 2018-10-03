9bdedd7f625cf7e5570794d204cf31c2d5924004
Derek Chafin <infomaniac50@gmail.com>
2018-10-03 17:31:30 -0500

    Wait for a key press before exiting in debug mode.

--- WFCSF/Program.cs
+++ WFCSF/Program.cs
@@ -5,13 +5,20 @@ namespace infomaniac50
 {
     class Program
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String)")]
         static int Main(string[] args)
         {
-            return Parser.Default.ParseArguments<WafOptions>(args)
+            int ExitCode = Parser.Default.ParseArguments<WafOptions>(args)
                 .MapResult(
                     (WafOptions opts) => RunWafAndReturnExitCode(opts),
                     errs => 1
                 );
+
+#if DEBUG
+            System.Console.WriteLine("Press any key to finish.");
+            System.Console.ReadKey();
+#endif
+            return ExitCode;
         }
     }
 }
